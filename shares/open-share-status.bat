d4ba5b611f04bb5efcff81f925b648f1c45ffdf8
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-20 11:38:43 +0530

    updates.

--- shares/open-share-status.bat
+++ shares/open-share-status.bat
@@ -2,6 +2,6 @@
 
 FOR /F "tokens=* delims=" %%x in (shares-list.txt) DO (
     echo opening %%x share price in google
-    @REM start chrome "https://www.google.com/search?q="+%%x+"+share"
+    start chrome "https://www.google.com/search?q="+%%x+"+share"
     @REM start firefox "https://www.google.com/search?q="+%%x+"+share"
 )
