075e978283e0292a168d7ca5c25475e74dccfe67
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-20 10:13:03 +0530

    initial commit.

---
+++ shares/open-share-status.bat
@@ -0,0 +1,7 @@
+@echo off
+
+FOR /F "tokens=* delims=" %%x in (shares-list.txt) DO (
+    echo opening %%x share price in google
+    @REM start chrome "https://www.google.com/search?q="+%%x+"+share"
+    @REM start firefox "https://www.google.com/search?q="+%%x+"+share"
+)
---
+++ test.bat
@@ -0,0 +1,9 @@
+@echo off
+
+FOR /F "tokens=* delims=" %%x in (new.txt) DO echo %%x
+
+Rem start chrome "https://www.google.com/search?q=naukri+share"
+Rem start chrome "https://www.google.com/search?q=nestle+india+share"
+
+Rem start "webpage name" "https://www.google.com/search?q=naukri+share"
+Rem start "webpage name" "https://www.google.com/search?q=nestle+india+share"
